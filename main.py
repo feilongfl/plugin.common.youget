eda52cec7e2d4a313e7987d8d12584c4e890aa44
feilong <feilongphone@gmail.com>
2019-01-10 17:27:06 +0800

    修改：     addon.xml 	修改：     main.py 	修改：     resources/lib/plugin.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
+# import sys
+# import xbmc
+#
+# if sys.version_info[0] > 2:
+#     xbmc.log('python 3')
+# else:
+#     xbmc.log('python 2')
 
-from resources.lib import kodilogging
+
+#from resources.lib import kodilogging
 from resources.lib import plugin
 
 import logging
 import xbmcaddon
 
+
+
 # Keep this file to a minimum, as Kodi
 # doesn't keep a compiled copy of this
 ADDON = xbmcaddon.Addon()
-kodilogging.config()
+#kodilogging.config()
 
 plugin.run()
-
-
--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -3,32 +3,79 @@
 import routing
 import logging
 import xbmcaddon
-from resources.lib import kodiutils
-from resources.lib import kodilogging
+import sys
+from you_get import common as youget
+from you_get import json_output as json_output_
 from xbmcgui import ListItem
 from xbmcplugin import addDirectoryItem, endOfDirectory
+import xbmc
+from urllib.parse import quote,unquote
 
 
 ADDON = xbmcaddon.Addon()
-logger = logging.getLogger(ADDON.getAddonInfo('id'))
-kodilogging.config()
 plugin = routing.Plugin()
-
 
 @plugin.route('/')
 def index():
     addDirectoryItem(plugin.handle, plugin.url_for(
-        show_category, "one"), ListItem("Category One"), True)
+        show_category, quote("https://www.bilibili.com/video/av38301694",safe='')), ListItem("FeiLong"), True)
     addDirectoryItem(plugin.handle, plugin.url_for(
-        show_category, "two"), ListItem("Category Two"), True)
+        show_category, quote("https://www.bilibili.com/video/av39523414",safe='')), ListItem("bilibili"), True)
+    addDirectoryItem(plugin.handle, plugin.url_for(
+        show_category, quote("https://v.youku.com/v_show/id_XNDAwNDI2MDkwMA==.html?spm=a2h1n.8251846.0.0",safe='')), ListItem("youku"), True)
     endOfDirectory(plugin.handle)
 
 
 @plugin.route('/category/<category_id>')
 def show_category(category_id):
-    addDirectoryItem(
-        plugin.handle, "", ListItem("Hello category %s!" % category_id))
-    endOfDirectory(plugin.handle)
+    print('cate=>', category_id)
+    playVideo(unquote(category_id))
+
+def download_urls(
+    urls, title, ext, total_size, output_dir='.', refer=None, merge=True,
+    faker=False, headers={}, **kwargs
+):
+    assert urls
+    print(urls)
+    url = urls[0]
+    for key, value in headers.items():
+        print (key,' => ',value)
+        if key != "User-Agent":
+            url = "%s|%s=%s" % (url,key,quote(value,safe=''))
+
+    playUrl(url)
+    return
+
+def playUrl(video_url):
+    print ('url => %s' % video_url)
+    playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+    playlist.clear()
+    li = ListItem(path=video_url)
+    li.setInfo( type="video", infoLabels={ "Path" : video_url } )
+    playlist.add(url=video_url, listitem=li)
+    xbmc.Player().play(playlist)
+
+def test_main(**kwargs):
+    youget.script_main(youget.any_download, youget.any_download_playlist, **kwargs)
+
+def playVideo(url):
+    # test_main()
+    extra = {}
+    # URLs = ['https://www.youtube.com/watch?v=AnwQAWmGgek']
+    # URLs = ['https://www.bilibili.com/video/av38301694']
+    URLs = [url]
+    # youget.set_socks_proxy("feilong-server.lan:10080")
+    youget.download_urls = download_urls
+    youget.download_main(
+        youget.any_download, youget.any_download_playlist,
+        # URLs, args.playlist,
+        URLs, False,
+        # output_dir=args.output_dir, merge=not args.no_merge,
+        output_dir='/tmp/', merge=True,
+        info_only=False, json_output=False,
+        # password=args.password,
+        **extra
+    )
 
 def run():
     plugin.run()
